9ae4b82906799a32e3f9fdde45e361107c797594
emrmason <mas20021@byui.edu>
2023-12-12 14:26:11 -0800

    Updated scores.js

--- src/js/scores.js
+++ src/js/scores.js
@@ -7,14 +7,3 @@ let accuracy = Math.ceil((score/moves)*100);
 let game = localStorage.getItem("game");
 
 document.getElementById("score-keeping").innerText = `Congratulations! You finished the ${game} game with ${accuracy}% accuracy!` 
-
-
-// function getStats() {
-//     score = parseInt(localStorage.getItem("score"));
-//     moves = parseInt(localStorage.getItem("moves"));
-//     let accuracy = score / moves;
-//     game = localStorage.getItem("game");
-    
-// }
-
-// getStats();
