7909441e8d4b6a1e06dd4acd59d6f44a524b3cc6
emrmason <mas20021@byui.edu>
2023-12-12 11:39:09 -0800

    Updated with Vite, Node, *Netlify??

---
+++ vite.config.js
@@ -0,0 +1,17 @@
+import { resolve } from "path";
+import { defineConfig } from "vite";
+
+export default defineConfig({
+  root: "src/",
+
+  build: {
+    outDir: "../dist",
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "src/index.html"),
+        memory: resolve(__dirname, "src/memory/index.html"),
+        trivia: resolve(__dirname, "src/trivia/index.html"),
+      },
+    },
+  },
+});
