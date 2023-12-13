2de82dd97e6015e8645278e993ca2651bb7a96ac
emrmason <mas20021@byui.edu>
2023-12-13 11:12:05 -0800

    Attempts to get npm run build working- FAIL

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,8 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
   root: "src/",
-
   build: {
-    outDir: "build",
+    outDir: "../dist",
     rollupOptions: {
       input: {
         main: resolve(__dirname, "src/index.html"),
